f01cd264c1ac18ca9b53344638e34ef434d22a4f
Ilham Malabu <malabuzainab@gmail.com>
2021-11-11 17:26:54 +0100

    added to db and changed color

--- src/data/data.js
+++ src/data/data.js
@@ -8,7 +8,168 @@ module.exports = [
     },
     {
         "id": 2,
-        "quote": "you've made even the me, who didn't believe in the existence of a god, believe that there's a god. to me, the goddess is you",
-        "person": "yoongi"
+        "quote": "I did then what I knew to do. Now that I know better I do better",
+        "person": "Maya Angelou"
+    },
+    {
+        "id": 3,
+        "quote": "You will face many defeats in life, but never let yourself be defeated",
+        "person": "Maya Angelou"
+    },
+    {
+        "id": 3,
+        "quote": "The thing women have yet to learn is nobody gives you power. You just take it",
+        "person": "Roseanne Barr"
+    },
+    {
+        "id": 4,
+        "quote": "Success is not final, failure is not fatal: it is the courage to continue that counts",
+        "person": "Winston Churchill"
+    },
+    {
+        "id": 5,
+        "quote": "A winner is a dreamer who never gives up",
+        "person": "Nelson Mandela"
+    },
+    {
+        "id": 6,
+        "quote": "It does not matter how slowly you go as long as you do not stop.",
+        "person": "Confucius"
+    },
+    {
+        "id": 7,
+        "quote": "You only live once, but if you do it right, once is enough",
+        "person": "Mae West"
+    },
+    {
+        "id": 8,
+        "quote": "Anyone who has never made a mistake has never tried anything new",
+        "person": "Albert Einstein"
+    },
+    {
+        "id": 9,
+        "quote": "If you can dream it, you can do it",
+        "person": "Walt Disney"
+    },
+    {
+        "id": 10,
+        "quote": "The time is always right to do what is right",
+        "person": "Martin Luther King Jr"
+    },
+    {
+        "id": 11,
+        "quote": "I did then what I knew to do. Now that I know better I do better",
+        "person": "Maya Angelou"
+    },
+    {
+        "id": 12,
+        "quote": "It isn't the mountains ahead to climb that wear you down. It's the pebble in your shoe",
+        "person": "Muhammad Ali"
+    },
+    {
+        "id": 13,
+        "quote": "You may not control all the events that happen to you, but you can decide not to be reduced by them",
+        "person": "Maya Angelou"
+    },
+    {
+        "id": 14,
+        "quote": "Believe you can and you're halfway there",
+        "person": "Theodore Roosevelt"
+    },
+    {
+        "id": 15,
+        "quote": "The greatest glory in living lies not in never falling, but in rising every time we fall",
+        "person": "Nelson Mandela"
+    },
+    {
+        "id": 16,
+        "quote": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+        "person": "Thomas Edison"
+    },
+    {
+        "id": 17,
+        "quote": "Difficulties increase the nearer we get to the goal.",
+        "person": "Johann Wolfgang von Goethe"
+    },
+    {
+        "id": 18,
+        "quote": "Fate is in your hands and no one elses",
+        "person": "Byron Pulsifer"
+    },
+    {
+        "id": 19,
+        "quote": "Nothing happens unless first we dream.",
+        "person": "Carl Sandburg"
+    },
+    {
+        "id": 20,
+        "quote": "Well begun is half done.",
+        "person": "Aristotle"
+    },
+    {
+        "id": 21,
+        "quote": "Having nothing, nothing can he lose.",
+        "person": "William Shakespeare"
+    },
+    {
+        "id": 22,
+        "quote": "Trust yourself. You know more than you think you do.",
+        "person": "Benjamin Spock"
+    },
+    {
+        "id": 23,
+        "quote": "Study the past, if you would divine the future.",
+        "person": "Confucius"
+    },
+    {
+        "id": 24,
+        "quote": "From error to error one discovers the entire truth.",
+        "person": "Sigmund Freud"
+    },
+    {
+        "id": 25,
+        "quote": "Well done is better than well said.",
+        "person": "Benjamin Franklin"
+    },
+    {
+        "id": 26,
+        "quote": "Once you choose hope, anythings possible.",
+        "person": "Christopher Reeve"
+    },
+    {
+        "id": 27,
+        "quote": "Looking for answers when there aren't any, but we still learn, isn't that what life is about?",
+        "person": "RM of BTS"
+    },
+    {
+        "id": 28,
+        "quote": "We all have lots to pitch in, there’s no knowing what will come. But hard work will get us somewhere",
+        "person": "Jungkook of BTS"
+    },
+    {
+        "id": 29,
+        "quote": "Happiness is not something you have to achieve, you can still feel happy during the process of achieving something",
+        "person": "RM of BTS"
+    },
+    {
+        "id": 30,
+        "quote": "May your trials end in full bloom",
+        "person": "Suga of BTS"
+    },
+    {
+        "id": 31,
+        "quote": "It's okay if you don't have a dream, you might not have one. Just being happy is fine",
+        "person": "Suga of BTS"
+    },
+    {
+        "id": 32,
+        "quote": "Those who keep trying without giving up are the ones who succeed",
+        "person": "Jin of BTS"
+    },
+    {
+        "id": 33,
+        "quote": "What's ordinary to others is special to me whereas what's special to others is very ordinary to me",
+        "person": "Suga of BTS"
     }
+
 ]
